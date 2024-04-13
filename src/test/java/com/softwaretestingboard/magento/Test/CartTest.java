3827113bbe6d8b6898bc2ec85bec629f655b6dd0
shahin <shahinpinjari41@gmail.com>
2024-04-13 21:20:10 +0530

    second project

---
+++ src/test/java/com/softwaretestingboard/magento/Test/CartTest.java
@@ -0,0 +1,25 @@
+package com.softwaretestingboard.magento.Test;
+
+import org.testng.annotations.Test;
+
+import com.softwaretestingboard.magento.Base.BaseClass;
+import com.softwaretestingboard.magento.Pages.CartPage;
+
+//@Listeners(com.softwaretestingboard.magento.listeners.Listners.class)
+public class CartTest extends BaseClass{
+	
+	CartPage cart;
+	
+  @Test
+  public void VerifyEditBtnInCart() {
+	  cart = new CartPage();
+	  cart.EditAddedProductinCart();
+  }
+  
+  @Test
+  public void VerifyRemoveProdFromCart() {
+	  cart = new CartPage();
+	  cart.RemoveAddedProductFromCart();
+  }
+  
+}
